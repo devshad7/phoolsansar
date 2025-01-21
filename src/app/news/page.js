21edefc99331a541ccafd01644d1219ceb0815c2
Md Shad Khan <devshaad@Mac.lan>
2025-01-21 23:43:53 +0530

    feat: fixed some error

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -5,11 +5,10 @@ import React from 'react'
 function page() {
   return (
     <>
-    {/* <Navbar />
+    <Navbar />
     <div className="absolute top-0 w-full h-screen flex justify-center items-center -z-30">
       <span>503 | Coming soon</span>
-    </div> */}
-    <BlogList />
+    </div>
     </>
   )
 }
